2085eec962783c3d49e7683f4281c6cd25cb3ffa
walter <walter.98.kong@gmail.com>
2022-09-15 09:32:00 +0800

    add fov proto

--- eComm/common/args.go
+++ eComm/common/args.go
@@ -9,6 +9,7 @@ type Arg struct {
 	EvalPort  uint
 	EvalKey   string
 	RelayPort uint
+	PynqPort  uint
 }
 
 func ParseArgs() Arg {
@@ -17,6 +18,7 @@ func ParseArgs() Arg {
 	flag.UintVar(&a.EvalPort, "evalPort", 8080, "Eval server port")
 	flag.StringVar(&a.EvalKey, "evalKey", "PLSPLSPLSPLSWORK", "Symmetric key for eval server")
 	flag.UintVar(&a.RelayPort, "relayPort", 8081, "Port to be SSH forwarded to relay")
+	flag.UintVar(&a.PynqPort, "pynqPort", 8082, "Port to send/poll signals")
 	flag.Parse()
 
 	// Checks
