2e4588f5bad41eeca0139218871dc252db3795a3
walter <walter.98.kong@gmail.com>
2022-09-15 12:33:40 +0800

    update icomm demo

--- iComm/main.py
+++ iComm/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import random
 import grpc
 import main_pb2
 import main_pb2_grpc
@@ -8,9 +9,12 @@ def run():
     with grpc.insecure_channel('localhost:8081') as channel:
         while True:
             input("Enter to send dummy packet...")
-            stub = main_pb2_grpc.RelayToUltraStub(channel)
-            msg = main_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
-            resp = stub.EmitFoo(msg)
+            stub = main_pb2_grpc.RelayStub(channel)
+            msg = main_pb2.SensorData(player=1)
+            if random.randint(1, 4) < 4:
+                resp = stub.Gesture(msg) # grenade, reload, shield
+            else:
+                resp = stub.Shoot(msg)
             print(f"Received resp {resp}")
 
 if __name__ == "__main__":
