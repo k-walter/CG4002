a3ad35ea06c9e576db5078c636301bb03baf44d0
walter <walter.98.kong@gmail.com>
2022-10-05 13:53:50 +0800

    WIP add engine actions

--- eComm/engine/engine.go
+++ eComm/engine/engine.go
@@ -3,8 +3,6 @@ package engine
 import (
 	"cg4002/eComm/common"
 	pb "cg4002/protos"
-	"fmt"
-	"google.golang.org/grpc"
 	"log"
 )
 
@@ -58,3 +56,85 @@ func (e *Engine) Close() {
 	// TODO Close channels
 }
 
+func (e *Engine) handleEvent(event *pb.Event) {
+	// Get player
+	player := func() *pb.PlayerState {
+		switch event.Player {
+		case 1:
+			return e.state.P1
+		case 2:
+			return e.state.P2
+		default:
+			log.Fatal("Unknown player ", event.Player)
+		}
+		return nil
+	}()
+
+	// Decide action
+	// TODO strategy pattern
+	switch event.Action {
+	case pb.Action_shoot:
+		if player.Bullets == 0 {
+			return
+		}
+		player.Bullets -= 1
+		// TODO add to shoot stream
+		// TODO resolve shoot-shot streams, inflict dmg, then fwd to viz
+
+	case pb.Action_shot:
+		const dmg = 10 // TODO clarify
+		// NOTE Always corresponds to some shoot
+		// TODO add to shot stream
+		// TODO resolve shoot-shot streams, inflict dmg, then fwd to viz
+
+	case pb.Action_grenade:
+		if player.Grenades == 0 {
+			return
+		}
+		player.Grenades -= 1
+
+		// Check opponent grenaded
+		// TODO viz to take req as action regardless
+		common.Pub(common.InFovReq, &pb.InFovMessage{
+			Player: 0b11 ^ event.Player, // other player
+			Time:   event.Time,
+			InFov:  true,
+		})
+
+	case pb.Action_grenaded:
+		const dmg = 10 // TODO clarify
+		inflict(player, dmg)
+
+	case pb.Action_reload:
+		fallthrough
+
+	case pb.Action_shield:
+		// TODO check timeout
+		fallthrough
+
+	case pb.Action_logout:
+		// TODO send logout signals
+		fallthrough
+
+	case pb.Action_none:
+		return
+
+	default:
+		log.Fatal("Unknown action", event.Action)
+	}
+}
+
+func inflict(player *pb.PlayerState, dmg uint32) {
+	if player.Hp+player.ShieldHealth <= dmg {
+		// Died
+		player.NumDeaths += 1
+		// TODO send died?
+	} else if player.ShieldHealth <= dmg {
+		// Dmg shield+health
+		player.Hp -= dmg - player.ShieldHealth
+		player.ShieldHealth = 0
+	} else {
+		// Dmg shield
+		player.ShieldHealth -= dmg
+	}
+}
