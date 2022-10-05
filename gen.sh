1d17fa2a77b9589af4f6005e7974dec891df9f5d
walter <walter.98.kong@gmail.com>
2022-10-05 13:31:38 +0800

    Generate grpc

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 set +xe
 
+# Generate go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 go get -u github.com/gin-gonic/gin
@@ -9,6 +10,10 @@ protoc --go_out=. --go_opt=paths=source_relative \
   --go-grpc_out=. --go-grpc_opt=paths=source_relative \
   protos/main.proto
 
+# Generate python
 python -m grpc_tools.protoc -I./protos \
   --python_out=./iComm --grpc_python_out=./iComm \
-  ./protos/main.proto
+  protos/main.proto
+python -m grpc_tools.protoc -I./protos \
+  --python_out=./eComm/pynq --grpc_python_out=./eComm/pynq \
+  protos/main.proto
