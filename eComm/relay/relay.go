55aa0c4292fc5b1ec7cd21ed63aa19c381a7ba2d
walter <walter.98.kong@gmail.com>
2022-10-05 13:50:40 +0800

    Verify iComm input and rename server

--- eComm/relay/relay.go
+++ eComm/relay/relay.go
@@ -6,7 +6,8 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 	"net"
@@ -26,31 +27,21 @@ func Make(a *common.Arg) *RelayServer {
 		log.Fatal(err)
 	}
 
-	// To pynq
-	pyConn, err := grpc.Dial(fmt.Sprintf(":%v", a.ToPynqPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil
-	}
-
-	return &RelayServer{
-		lis:    lis,
-		py:     pb.NewToPynqClient(pyConn),
-		pyConn: pyConn,
-	}
+	return &RelayServer{lis: lis}
 }
 
 func (s *RelayServer) Run() {
 	g := grpc.NewServer()
-	pb.RegisterFromRelayServer(g, s)
-
+	pb.RegisterRelayServer(g, s)
 	if err := g.Serve(s.lis); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("running relay")
+
 }
 
 func (s *RelayServer) Close() {
 	_ = s.lis.Close()
-	_ = s.pyConn.Close()
 }
 
 func (s *RelayServer) Gesture(c context.Context, d *pb.SensorData) (*emptypb.Empty, error) {
@@ -62,20 +53,36 @@ func (s *RelayServer) Gesture(c context.Context, d *pb.SensorData) (*emptypb.Emp
 
 func (s *RelayServer) Shoot(c context.Context, e *pb.Event) (*emptypb.Empty, error) {
 	log.Println("relay|Received shoot")
-	common.Pub(common.EventToEngine, pb.Event{
-		Player: d.Player,
-		Time:   uint32(time.Now().Nanosecond()),
-		Action: pb.Action_shoot,
-	})
+
+	// Verification
+	if !(1 <= e.Player && e.Player <= 2) {
+		return nil, status.Error(codes.Unknown, "Player must be 1/2")
+	}
+	if e.Action != pb.Action_shoot {
+		return nil, status.Error(codes.Unknown, "Shoot() called with non-shoot action")
+	}
+
+	// Forward to engine
+	e.Time = uint32(time.Now().Nanosecond())
+	common.Pub(common.Event2Eng, e)
+
 	return &emptypb.Empty{}, nil
 }
 
 func (s *RelayServer) Shot(c context.Context, e *pb.Event) (*emptypb.Empty, error) {
 	log.Println("relay|Received shot")
-	common.Pub(common.EventToEngine, pb.Event{
-		Player: d.Player,
-		Time:   uint32(time.Now().Nanosecond()),
-		Action: pb.Action_shot,
-	})
+
+	// Verification
+	if !(e.Player == 1 || e.Player == 2) {
+		return nil, status.Error(codes.Unknown, "Player must be 1/2")
+	}
+	if e.Action != pb.Action_shot {
+		return nil, status.Error(codes.Unknown, "Shot() called with non-shot action")
+	}
+
+	// Forward to engine
+	e.Time = uint32(time.Now().Nanosecond())
+	common.Pub(common.Event2Eng, e)
+
 	return &emptypb.Empty{}, nil
 }
