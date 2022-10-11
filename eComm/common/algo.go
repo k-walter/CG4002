f0f3cb54c55dd4bd52731709a9fa195a8aefcb0e
walter <walter.98.kong@gmail.com>
2022-10-12 00:02:53 +0800

    Add helper algo

---
+++ eComm/common/algo.go
@@ -0,0 +1,47 @@
+package common
+
+import "log"
+
+type Number interface {
+	uint64
+}
+
+func Max[T Number](i, j T) T {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func Min[T Number](i, j T) T {
+	if i < j {
+		return i
+	}
+	return j
+}
+
+func AbsDiff[T Number](i, j T) T {
+	return Max(i, j) - Min(i, j)
+}
+
+func EXIT_UNLESS(b bool) {
+	if !b {
+		log.Panicln()
+	}
+}
+
+// Returns index if found, else -1
+func BinarySearch[T Number](a []T, v T) int {
+	for l, r := 0, len(a); l < r; {
+		m := (l + r) >> 1
+		if a[m] == v {
+			return m
+		}
+		if a[m] < v {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return -1
+}
