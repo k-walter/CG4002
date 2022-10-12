5b2bc140fd5f4774f30dbecaada8993595347c02
walter <walter.98.kong@gmail.com>
2022-10-12 16:32:46 +0800

    Update proto for index and shoot ID

--- eComm/pynq/main_pb2.py
+++ eComm/pynq/main_pb2.py
@@ -16,7 +16,7 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nmain.proto\x1a\x1bgoogle/protobuf/empty.proto\"u\n\nSensorData\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\x0c\n\x04roll\x18\x03 \x01(\r\x12\r\n\x05pitch\x18\x04 \x01(\r\x12\x0b\n\x03yaw\x18\x05 \x01(\r\x12\t\n\x01x\x18\x06 \x01(\r\x12\t\n\x01y\x18\x07 \x01(\r\x12\t\n\x01z\x18\x08 \x01(\r\"8\n\tInFovResp\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\r\n\x05inFov\x18\x03 \x01(\x08\">\n\x05\x45vent\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\x17\n\x06\x61\x63tion\x18\x03 \x01(\x0e\x32\x07.Action\"C\n\x05State\x12\x1c\n\x02p1\x18\x01 \x01(\x0b\x32\x0c.PlayerStateR\x02p1\x12\x1c\n\x02p2\x18\x02 \x01(\x0b\x32\x0c.PlayerStateR\x02p2\"\xfc\x01\n\x0bPlayerState\x12\x0e\n\x02hp\x18\x01 \x01(\rR\x02hp\x12\x1f\n\x06\x61\x63tion\x18\x02 \x01(\x0e\x32\x07.ActionR\x06\x61\x63tion\x12\x18\n\x07\x62ullets\x18\x03 \x01(\rR\x07\x62ullets\x12\x1a\n\x08grenades\x18\x04 \x01(\rR\x08grenades\x12 \n\x0bshield_time\x18\x05 \x01(\x01R\x0bshield_time\x12$\n\rshield_health\x18\x06 \x01(\rR\rshield_health\x12\x1e\n\nnum_deaths\x18\x07 \x01(\rR\nnum_deaths\x12\x1e\n\nnum_shield\x18\x08 \x01(\rR\nnum_shield*m\n\x06\x41\x63tion\x12\x08\n\x04none\x10\x00\x12\x0b\n\x07grenade\x10\x01\x12\n\n\x06reload\x10\x02\x12\t\n\x05shoot\x10\x03\x12\n\n\x06logout\x10\x04\x12\n\n\x06shield\x10\x05\x12\x08\n\x04shot\x10\x06\x12\x13\n\x0fshieldAvailable\x10\x07\x32\x8e\x01\n\x05Relay\x12\x30\n\x07Gesture\x12\x0b.SensorData\x1a\x16.google.protobuf.Empty\"\x00\x12)\n\x05Shoot\x12\x06.Event\x1a\x16.google.protobuf.Empty\"\x00\x12(\n\x04Shot\x12\x06.Event\x1a\x16.google.protobuf.Empty\"\x00\x32P\n\x03Viz\x12*\n\x06Update\x12\x06.State\x1a\x16.google.protobuf.Empty\"\x00\x12\x1d\n\x05InFov\x12\x06.Event\x1a\n.InFovResp\"\x00\x32O\n\x04Pynq\x12\x1d\n\x04\x45mit\x12\x0b.SensorData\x1a\x06.Event\"\x00\x12(\n\x04Poll\x12\x16.google.protobuf.Empty\x1a\x06.Event\"\x00\x42\x04Z\x02./b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nmain.proto\x1a\x1bgoogle/protobuf/empty.proto\"\x84\x01\n\nSensorData\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\x0c\n\x04roll\x18\x03 \x01(\r\x12\r\n\x05pitch\x18\x04 \x01(\r\x12\x0b\n\x03yaw\x18\x05 \x01(\r\x12\t\n\x01x\x18\x06 \x01(\r\x12\t\n\x01y\x18\x07 \x01(\r\x12\t\n\x01z\x18\x08 \x01(\r\x12\r\n\x05index\x18\t \x01(\r\"8\n\tInFovResp\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\r\n\x05inFov\x18\x03 \x01(\x08\"O\n\x05\x45vent\x12\x0e\n\x06player\x18\x01 \x01(\r\x12\x0c\n\x04time\x18\x02 \x01(\x04\x12\x17\n\x06\x61\x63tion\x18\x03 \x01(\x0e\x32\x07.Action\x12\x0f\n\x07shootID\x18\x04 \x01(\r\"C\n\x05State\x12\x1c\n\x02p1\x18\x01 \x01(\x0b\x32\x0c.PlayerStateR\x02p1\x12\x1c\n\x02p2\x18\x02 \x01(\x0b\x32\x0c.PlayerStateR\x02p2\"\xfc\x01\n\x0bPlayerState\x12\x0e\n\x02hp\x18\x01 \x01(\rR\x02hp\x12\x1f\n\x06\x61\x63tion\x18\x02 \x01(\x0e\x32\x07.ActionR\x06\x61\x63tion\x12\x18\n\x07\x62ullets\x18\x03 \x01(\rR\x07\x62ullets\x12\x1a\n\x08grenades\x18\x04 \x01(\rR\x08grenades\x12 \n\x0bshield_time\x18\x05 \x01(\x01R\x0bshield_time\x12$\n\rshield_health\x18\x06 \x01(\rR\rshield_health\x12\x1e\n\nnum_deaths\x18\x07 \x01(\rR\nnum_deaths\x12\x1e\n\nnum_shield\x18\x08 \x01(\rR\nnum_shield*m\n\x06\x41\x63tion\x12\x08\n\x04none\x10\x00\x12\x0b\n\x07grenade\x10\x01\x12\n\n\x06reload\x10\x02\x12\t\n\x05shoot\x10\x03\x12\n\n\x06logout\x10\x04\x12\n\n\x06shield\x10\x05\x12\x08\n\x04shot\x10\x06\x12\x13\n\x0fshieldAvailable\x10\x07\x32\x8e\x01\n\x05Relay\x12\x30\n\x07Gesture\x12\x0b.SensorData\x1a\x16.google.protobuf.Empty\"\x00\x12)\n\x05Shoot\x12\x06.Event\x1a\x16.google.protobuf.Empty\"\x00\x12(\n\x04Shot\x12\x06.Event\x1a\x16.google.protobuf.Empty\"\x00\x32P\n\x03Viz\x12*\n\x06Update\x12\x06.State\x1a\x16.google.protobuf.Empty\"\x00\x12\x1d\n\x05InFov\x12\x06.Event\x1a\n.InFovResp\"\x00\x32O\n\x04Pynq\x12\x1d\n\x04\x45mit\x12\x0b.SensorData\x1a\x06.Event\"\x00\x12(\n\x04Poll\x12\x16.google.protobuf.Empty\x1a\x06.Event\"\x00\x42\x04Z\x02./b\x06proto3')
 
 _ACTION = DESCRIPTOR.enum_types_by_name['Action']
 Action = enum_type_wrapper.EnumTypeWrapper(_ACTION)
@@ -77,22 +77,22 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'Z\002./'
-  _ACTION._serialized_start=608
-  _ACTION._serialized_end=717
-  _SENSORDATA._serialized_start=43
-  _SENSORDATA._serialized_end=160
-  _INFOVRESP._serialized_start=162
-  _INFOVRESP._serialized_end=218
-  _EVENT._serialized_start=220
-  _EVENT._serialized_end=282
-  _STATE._serialized_start=284
-  _STATE._serialized_end=351
-  _PLAYERSTATE._serialized_start=354
-  _PLAYERSTATE._serialized_end=606
-  _RELAY._serialized_start=720
-  _RELAY._serialized_end=862
-  _VIZ._serialized_start=864
-  _VIZ._serialized_end=944
-  _PYNQ._serialized_start=946
-  _PYNQ._serialized_end=1025
+  _ACTION._serialized_start=641
+  _ACTION._serialized_end=750
+  _SENSORDATA._serialized_start=44
+  _SENSORDATA._serialized_end=176
+  _INFOVRESP._serialized_start=178
+  _INFOVRESP._serialized_end=234
+  _EVENT._serialized_start=236
+  _EVENT._serialized_end=315
+  _STATE._serialized_start=317
+  _STATE._serialized_end=384
+  _PLAYERSTATE._serialized_start=387
+  _PLAYERSTATE._serialized_end=639
+  _RELAY._serialized_start=753
+  _RELAY._serialized_end=895
+  _VIZ._serialized_start=897
+  _VIZ._serialized_end=977
+  _PYNQ._serialized_start=979
+  _PYNQ._serialized_end=1058
 # @@protoc_insertion_point(module_scope)
